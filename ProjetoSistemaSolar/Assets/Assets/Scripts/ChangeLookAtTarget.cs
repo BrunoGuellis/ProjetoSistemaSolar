5ea98e9f88c41bc00f91f440936940b5ab55c90a
BrunoGuellis <brunoguellisdealmeida@gmail.com>
2017-11-09 10:22:11 -0200

    projetoJogoRoller

---
+++ ProjetoSistemaSolar/Assets/Assets/Scripts/ChangeLookAtTarget.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeLookAtTarget : MonoBehaviour {
+
+	public GameObject target; // the target that the camera should look atx
+    //public AudioSource audio;
+
+    void Start() {
+		if (target == null) 
+		{
+			target = this.gameObject;
+			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+
+        
+	}
+
+	// Called when MouseDown on this gameObject
+	void OnMouseDown () {
+		// change the target of the LookAtTarget script to be this gameobject.
+        mutaTudo();
+        LookAtTarget.target = target;
+        GameObject.Find(this.name).GetComponent<AudioSource>().enabled = true;
+        Camera.main.fieldOfView = 60 * target.transform.localScale.x;
+        
+    }
+
+    void mutaTudo()
+    {
+        GameObject[] planetas = GameObject.FindGameObjectsWithTag("planetas");
+
+        for (int i=0; i < planetas.Length; i++)
+            planetas[i].GetComponent<AudioSource>().enabled = false;
+    }
+}
---
+++ RollerMdness/Assets/Scripts/TimerLava.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerLava : MonoBehaviour {
+    float timeleft;
+    public GameObject go;
+    public GameObject chaozera;
+    public GameObject lavera;
+    public GameObject bumper;
+    public GameObject bumperd;
+    public GameObject bumpert;
+    public GameObject bumperq;
+    public GameObject bumperc;
+    public Text tempo;
+    public int ponto =0;
+    // Use this for initialization
+    void Start () {
+        timeleft = 10f;
+	}
+
+
+    // Update is called once per frame
+    void Update () {
+        timeleft -= Time.deltaTime;
+        if (timeleft > 0)
+            {
+            
+            tempo.text = timeleft.ToString()+"                    |                    "+ponto;
+            }
+        else {
+            Debug.Log(timeleft);
+            chaozera.SetActive(false);
+            lavera.SetActive(true);
+            if (timeleft < -3)
+            {
+                chaozera.SetActive(true);
+                lavera.SetActive(false);
+                timeleft = 10;
+                ponto++;
+                bumper.transform.position = new Vector3(Random.Range(-16, 16),1, Random.Range(-16, 16));
+                bumperd.transform.position = new Vector3(Random.Range(-16, 16),1, Random.Range(-16, 16));
+                bumpert.transform.position = new Vector3(Random.Range(-16, 16),1, Random.Range(-16, 16));
+                bumperq.transform.position = new Vector3(Random.Range(-16, 16),1, Random.Range(-16, 16));
+                bumperc.transform.position = new Vector3(Random.Range(-16, 16),1, Random.Range(-16, 16));
+                
+
+            }
+        }
+
+        
+    }
+}
